6718a56badc908dece99a8d8da52cbeef83f44c2
01subhashree <ssubhashree094@gmail.com>
2023-05-04 10:50:08 +0530

    day1

--- gym-project-2.0/src/components/footer/Footer.jsx
+++ gym-project-2.0/src/components/footer/Footer.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import style from "./Footer.module.css";
+import { Link } from "react-router-dom";
+import { ImFacebook2, ImYoutube } from "react-icons/im";
+import { BsLinkedin } from "react-icons/bs";
+import { ImWhatsapp } from "react-icons/im";
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className={style.container}>
+        <div className={style.footer_title}>
+          <p>The Warehouse Gym</p>
+          <p>Your Body Temple</p>
+
+          <div className={style.social_icons}>
+            <Link to={"https://www.facebook.com/"}>
+              <ImFacebook2 className={style.icons} />
+            </Link>
+            <Link to={"https://www.linkedin.com/"}>
+              <BsLinkedin className={style.icons} />
+            </Link>
+            <Link to={"https://twitter.com/"}>
+              <ImWhatsapp className={style.icons} />
+            </Link>
+            <Link to={"https://www.youtube.com/"}>
+              <ImYoutube className={style.icons} />
+            </Link>
+          </div>
+        </div>
+
+        <div className={style.footer_content}>
+          <hr />
+          <ul>
+            <h3>The Warehouse Gym</h3>
+            <Link to="/home">Home</Link>
+            <Link to="/about">AboutUs</Link>
+            <Link to="/training">Traning</Link>
+          </ul>
+
+          <ul>
+            <h3>Services</h3>
+            <Link to="/pricing">Apply Online</Link>
+            <Link>Available Positions</Link>
+          </ul>
+
+          <ul>
+            <p>About Us</p>
+            <Link to="/trainer">Meet Our Team</Link>
+            <Link>Our Responsibilities</Link>
+            <Link>Our Codes</Link>
+            <Link>Our Values</Link>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- gym-project-2.0/src/pages/about-us/AboutUs.jsx
+++ gym-project-2.0/src/pages/about-us/AboutUs.jsx
@@ -1,20 +1,36 @@
+import style from "./AboutUs.module.css";
+import { Link } from "react-router-dom";
+
 function AboutUs() {
   return (
-    <div>
-      <iframe
-        src="https://youtube.com/embed/Z3TdDTG97j8"
-        frameBorder="0"
-        allowFullScreen={true}
-        title="Gym video1"
-      ></iframe>
-      <p>this is the video</p>
-      <p>this is the video</p>
-      <iframe
-        src="https://youtube.com/embed/C6mnw1lMGOY"
-        frameBorder="0"
-        allowFullScreen={true}
-        title="Gym video2"
-      ></iframe>
+    <div className={style.Maindiv}>
+      <div className={style.container1}>
+        <span>
+          <iframe
+            src="https://youtube.com/embed/Z3TdDTG97j8"
+            frameBorder="0"
+            allowFullScreen={true}
+            title="Gym video1"
+          ></iframe>
+        </span>
+        <span>
+          <p>this is the video</p>
+          <button>
+            <Link to={"/program"}>Learn More</Link>
+          </button>
+        </span>
+      </div>
+      <div className={style.container2}>
+        <p>this is the video</p>
+        <span>
+          <iframe
+            src="https://youtube.com/embed/C6mnw1lMGOY"
+            frameBorder="0"
+            allowFullScreen={true}
+            title="Gym video2"
+          ></iframe>
+        </span>
+      </div>
     </div>
   );
 }
