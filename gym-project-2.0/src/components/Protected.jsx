b71faeea7c6e27dd763e81fad15abd6bf7deac2e
Rohit-2012 <rohit.kirti1219@gmail.com>
2023-05-06 23:20:42 +0530

    fixed isLoggedIn

--- gym-project-2.0/src/components/Protected.jsx
+++ gym-project-2.0/src/components/Protected.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from "react-router-dom"
 import { useRecoilValue } from 'recoil'
-import {loginStat} from './Atom'
+// import {loginStat} from './Atom'
 
 function Protected({Component}) {
 
-  const isloggedin = useRecoilValue(loginStat)
-    const navigate = useNavigate()
+  // const isloggedin = useRecoilValue(loginStat)
+  //   const navigate = useNavigate()
 
-    useEffect(()=>{
-        // let login = localStorage.getItem('loginStat')
-        if(isloggedin===false ){
-            navigate('/joining')
-        }
-    })
+  //   useEffect(()=>{
+  //       let login = localStorage.getItem('loginStat')
+  //       if(isloggedin===false ){
+  //           navigate('/joining')
+  //       }
+  //   })
     
   return (
     <div>
